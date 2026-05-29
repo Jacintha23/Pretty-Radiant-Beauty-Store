ef2a1dca8ca4487f130899da921839351350987d
Jacintha Gardner <jacintha.gardner6@gmail.com>
2026-05-28 22:10:09 -0700

    Added toString method in Brush class

--- src/main/java/com/pluralsight/Brush.java
+++ src/main/java/com/pluralsight/Brush.java
@@ -15,4 +15,38 @@ public class Brush extends Order
     }
 
 
+    public String brushSize()
+    {
+        return brushSize;
+    }
+
+    public double standardBSize()
+    {
+        return standardBSize;
+    }
+
+    public double standardBPrice()
+    {
+        return standardBPrice;
+    }
+
+    public double getTotal()
+    {
+        double total = 0;
+
+        switch(brushSize.toUpperCase())
+        {
+            case "S":
+                total =+ standardBPrice;
+                break;
+        }
+        return total;
+    }
+
+    public String toString()
+    {
+        // public Brush(String brushSize)
+        return this.brushSize + " | " +this.getTotal();
+    };
 }
+
