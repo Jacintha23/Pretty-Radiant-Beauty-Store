6d1d4dd1d6826bcc38e2c6cf050666baa9ef9f35
Jacintha Gardner <jacintha.gardner6@gmail.com>
2026-05-28 22:09:12 -0700

    Created Checkout Class

--- src/main/java/com/pluralsight/Checkout.java
+++ src/main/java/com/pluralsight/Checkout.java
@@ -1,12 +1,22 @@
 package com.pluralsight;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Checkout
 {
+        private ArrayList<Foundation> foundations;
+        private ArrayList<Powder> powders;
+        private ArrayList<Brush> brushes;
+        private Scanner scanner = new Scanner(System.in);
 
-
-
-
-
+    public Checkout(ArrayList<Foundation> foundations, ArrayList<Powder> powders, ArrayList<Brush> brushes, Scanner scanner)
+    {
+        this.foundations = foundations;
+        this.powders = powders;
+        this.brushes = brushes;
+        this.scanner = scanner;
+    }
 
 
 
